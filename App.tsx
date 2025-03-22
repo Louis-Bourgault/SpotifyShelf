07d1c346ea455da91f1f1fe7b36ef490529ca2df
modamo-gh <morganthemosaic@gmail.com>
2025-03-22 00:01:30 -0400

    Change from Flatlist to four static views

--- App.tsx
+++ App.tsx
@@ -1,76 +1,47 @@
 import React from "react";
 import {
-  FlatList,
   Image,
   ImageBackground,
   SafeAreaView,
   StyleSheet,
-  Text,
   View
 } from "react-native";
 
 const albumCover =
 	"https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg";
 
-const data: Array<ShelfProps> = [
-	[
-		{ name: "The Beatles", image: albumCover, id: "1" },
-		{ name: "The Beatles", image: albumCover, id: "2" },
-		{ name: "The Beatles", image: albumCover, id: "3" }
-	],
-	[
-		{ name: "The Beatles", image: albumCover, id: "4" },
-		{ name: "The Beatles", image: albumCover, id: "5" },
-		{ name: "The Beatles", image: albumCover, id: "6" }
-	],
-	[
-		{ name: "The Beatles", image: albumCover, id: "7" },
-		{ name: "The Beatles", image: albumCover, id: "8" },
-		{ name: "The Beatles", image: albumCover, id: "9" }
-	]
+const albums: any[] = [
+	{ name: "The Beatles", image: albumCover, id: "1" },
+	{ name: "The Beatles", image: albumCover, id: "2" },
+	{ name: "The Beatles", image: albumCover, id: "3" }
 ];
-
-function Shelf({ shelf }: { shelf: ShelfProps }) {
-	return (
-		<View style={styles.shelfContainer}>
-			{/* Row of album covers */}
-			<View style={styles.row}>
-				{shelf.map((item) => (
-					<View key={item.id} style={styles.albumContainer}>
-						<Image
-							source={{ uri: item.image }}
-							style={styles.albumCover}
-						/>
-						<Text style={styles.albumText}>{item.name}</Text>
-					</View>
-				))}
-			</View>
-			{/* Shelf image below the row. This currently doesn't work for some reason, I've got more important things to fix now. */}
-			{/* <Image source={require('./assets/shelf.jpg')} style={styles.shelf} /> */}
-		</View>
-	);
-}
 
 export default function App() {
 	return (
 		<ImageBackground
-			source={require("./assets/shelfBackground.png")} // Ensure the image is in the correct path
+			source={require("./assets/shelf.png")} // Ensure the image is in the correct path
 			style={styles.background}
 			resizeMode="cover"
 		>
 			<SafeAreaView style={styles.container}>
-				<FlatList
-					data={data}
-					keyExtractor={(_, index) => index.toString()}
-					renderItem={({ item }) => <Shelf shelf={item} />}
-					contentContainerStyle={styles.list}
-				/>
+				<View style={{ height: "70%" }}>
+					{Array.from({ length: 4 }).map((element) => (
+						<View style={styles.shelf}>
+							{albums.map((album) => (
+								<View style={styles.albumContainer}>
+									<Image
+										source={{ uri: album.image }}
+										style={styles.albumCover}
+									/>
+								</View>
+							))}
+						</View>
+					))}
+				</View>
 			</SafeAreaView>
 		</ImageBackground>
 	);
 }
-
-type ShelfProps = Array<{ name: string; image: string; id: string }>;
 
 const styles = StyleSheet.create({
 	background: {
@@ -96,8 +67,9 @@ const styles = StyleSheet.create({
 		alignItems: "center"
 	},
 	albumCover: {
-		width: 110,
-		height: 110,
+		backgroundColor: "blue",
+		width: 100,
+		height: 100,
 		borderRadius: 5
 	},
 	albumText: {
@@ -107,9 +79,10 @@ const styles = StyleSheet.create({
 		color: "white" // Ensure text is visible on the dark background
 	},
 	shelf: {
-		width: "100%",
-		height: 30,
-		resizeMode: "contain" // Ensure the shelf image scales properly
+		display: "flex",
+		flexDirection: "row",
+		height: "25%",
+		justifyContent: "space-evenly"
 	},
 	shelfContainer: {
 		width: "95%",
