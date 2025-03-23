cc991bf35ee1cc3fdbbceec395deb5f1287bbb30
modamo-gh <morganthemosaic@gmail.com>
2025-03-22 23:49:25 -0400

    Expand album

--- Album.tsx
+++ Album.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
-import { Image, Pressable } from "react-native";
+import React, { useRef } from "react";
+import { Image, Pressable, View } from "react-native";
 
-const Album: React.FC<{ albumIndex: number; imageURL: string }> = ({
-	albumIndex,
-	imageURL
-}) => {
-	const [isSelected, setIsSelected] = useState(false);
+const Album: React.FC<{
+	albumIndex: number;
+	imageURL: string;
+	onPress: (
+		index: number,
+		layout: { x: number; y: number; width: number; height: number }
+	) => void;
+}> = ({ albumIndex, imageURL, onPress }) => {
+	const albumRef = useRef<View>(null);
+
+	const handlePress = () => {
+		albumRef.current?.measure((x, y, height, width, pageX, pageY) => {
+			onPress(albumIndex, { x: pageX, y: pageY, width, height });
+		});
+	};
 
 	return (
 		<Pressable
 			key={albumIndex}
-			onPress={() => setIsSelected((prev) => !prev)}
-			style={{ alignItems: "center" }}
+			onPress={handlePress}
+            ref={albumRef}
+			style={{
+				alignItems: "center"
+			}}
 		>
 			<Image
 				source={{ uri: imageURL }}
